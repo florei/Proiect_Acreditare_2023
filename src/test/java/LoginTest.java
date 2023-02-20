5ea8c8168e9dda727038c71f8831b5a6b3d35b8e
Florentina Alexa <florentina.alexa6@gmail.com>
2023-02-21 00:45:32 +0200

    Proiect acreditare inceput

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+
+    @Test
+    public void loginWithValidData(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("http://qa5.fasttrackit.org:8008/");
+
+        WebElement accountLink = driver.findElement(By.cssSelector("#menu-item-70 > a"));
+        accountLink.click();
+        driver.findElement(By.id("username")).sendKeys("florentina.alexa6@gmail.com");
+        driver.findElement(By.id("password")).sendKeys("Programare.QA29");
+
+        wait(6);
+
+        driver.findElement(By.cssSelector("#customer_login > div.u-column1.col-1 > form > p:nth-child(3) > button")).click();
+
+
+        driver.close();
+
+    }
+
+    public static void wait (int seconds){
+        try {
+            Thread.sleep(seconds*1000L);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
